1e78d162774096442125d49075b4f2fefc02b230
KULBHUSHAN <krishnathakur0007895@gmail.com>
2023-11-01 10:51:37 +0530

    Project setup completed

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+
+app.set("view engine", "ejs");
+app.set("views", path.resolve("./views"));
+
+
+
+
+const app = express();
+const PORT = process.env.PORT || 3000;
