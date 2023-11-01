35382f6bd886ff52bb8228574e486bc33591acfd
KULBHUSHAN <krishnathakur0007895@gmail.com>
2023-11-01 13:49:57 +0530

    Added routes, connected DB and successful authentication

--- app.js
+++ app.js
@@ -1,11 +1,46 @@
-const express = require("express");
+require("dotenv").config();
 
+const path = require("path");
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+
+const Blog = require("./models/blog");
+
+const userRoute = require("./routes/user");
+const blogRoute = require("./routes/blog");
+
+const {
+  checkForAuthenticationCookie,
+} = require("./middlewares/authentication");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+// const PORT = 3001;
+
+mongoose.set('strictQuery', false);
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then((e) => console.log("MongoDB Connected"));
 
 app.set("view engine", "ejs");
 app.set("views", path.resolve("./views"));
 
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(checkForAuthenticationCookie("token"));
+app.use(express.static(path.resolve("./public")));
+// app.use(checkForAuthenticationCookie.setUser);
 
+app.get("/", async (req, res) => { 
+  const allBlogs = await Blog.find({});
+  res.render("home", {
+    user: req.user,
+    blogs: allBlogs,
+  });
+});
 
+app.use("/user", userRoute);
+app.use("/blog", blogRoute);
 
-const app = express();
-const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server Started at PORT:${PORT}`));
